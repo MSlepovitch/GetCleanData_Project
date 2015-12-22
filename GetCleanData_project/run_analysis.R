7635497ec450003ec66c13cc848386c284e3cc21
MSlepovitch <MSlepovitch@users.noreply.github.com>
2015-12-22 09:19:21 -0500

    Update run_analysis.R

--- GetCleanData_project/run_analysis.R
+++ GetCleanData_project/run_analysis.R
@@ -18,17 +18,12 @@ run_analysis.R <- function (directory){
   
   ## Extract only the mean and standard deviation data
   
-    ## Features 1-200 are 5 sets of 40 each, varying by XYZ, the means and stds are the first 6 features in each set
-    ind<-rep(seq(0,160,by=40),6)+rep(c(1:6), each = 5)
-    ## Features 201-265 are 5 sets of 13 each, the means and stds are the first 2 features in each set
-    ind<-c(ind,rep(seq(200,254,by=13),2)+rep(c(1:2), each = 5))
-    ## Features 266-502 are 3 sets of 79 each, varying by XYZ, the means and stds are the first 6 features in each set
-    ind<-c(ind,rep(seq(265,423,by=79),6)+rep(c(1:6), each = 3))
-    ## Features 503-554 are 4 sets of 13 each, the means and stds are the first 2 features in each set
-    ind<-sort(c(ind,rep(seq(502,541,by=13),2)+rep(c(1:2), each = 4)), decreasing = FALSE)
+    substring="mean|std"
+    FeatureNames<-(read.table (paste(directory,"/features.txt", sep = ""),sep=""))[,"V2", drop = FALSE]
   
-    X_combine_mean_std<-X_combine[,ind]
+    X_combine_mean_std<-X_combine[,grep(substring,FeatureNames[,1])]
   
+    
   ## assign descriptive activity names to the activities in the data set
   
     act_names<-(read.table (paste(directory,"/activity_labels.txt", sep = ""),sep=""))[,"V2", drop = FALSE]
@@ -36,19 +31,16 @@ run_analysis.R <- function (directory){
   
   ## label the data set with descriptive variable names
   
-    FeatureNames<-(read.table (paste(directory,"/features.txt", sep = ""),sep=""))[,"V2", drop = FALSE]
-    FeatureNames<-FeatureNames[ind,]
-  
     colnames(subject_combine)<-"subject"
-    colnames(X_combine_mean_std)<-FeatureNames
+    colnames(X_combine_mean_std)<-grep(substring,FeatureNames[,1], value=TRUE)
   
-    CleanData<-cbind(subject_combine,activities,X_combine_mean_std, deparse.level = 0, make.row.names = FALSE)
+    CleanData<-cbind(subject_combine,activities,X_combine_mean_std, deparse.level = 0)
   
   ## create a second, independent tidy data set with the average of each variable for each activity and each subject
   
-    headings<-c("Subject", "Activity", paste("Avg_",FeatureNames,sep=""))
+    headings<-c("Subject", "Activity", paste("Avg_",grep(substring,FeatureNames[,1], value=TRUE),sep=""))
   
-    TidyData<-aggregate(CleanData[,c(3:68)],list(CleanData$subject, CleanData$activities), mean)
+    TidyData<-aggregate(CleanData[,c(3:dim(CleanData)[2])],list(CleanData$subject, CleanData$activities), mean)
     colnames(TidyData)<-headings
   
   write.table(TidyData, file=paste(directory,"/TidyDataSet.txt", sep=""), row.name=FALSE)
